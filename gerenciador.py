077803a401a457c2db9e64137c09844c4f48dca0
claudiopintofilho <claudio.pinto.lopes.filho@ccc.ufcg.edu.br>
2026-05-28 11:55:41 -0300

    Fase3 Do Lab Finalizado

--- gerenciador.py
+++ gerenciador.py
@@ -1,6 +1,12 @@
 import json
 import os
 import hashlib
+import base64 
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
+_CHAVE_CRIPTOGRAFICA = None
 
 ARQUIVOS_DADOS = "dados_criptografados.json"
 
@@ -18,13 +24,26 @@ def salvar_dados(dados):
         json.dump(dados, arquivo, indent=4)
 
 def adicionar_senha(servico, usuario, senha_pura):
+    global _CHAVE_CRIPTOGRAFICA
+    if _CHAVE_CRIPTOGRAFICA is None:
+        print("⚠️Erro: Sessão não iniciada. Autentique-se primeiro.")
+        return
+    
     dados = ler_dados()
     if "credenciais" not in dados:
         dados["credenciais"] = {}
     
+    #inicializa o motor de criptografia com a nossa chave da memoria RAM
+    f = Fernet(_CHAVE_CRIPTOGRAFICA)
+    senha_bytes = senha_pura.encode('utf-8')
+    senha_criptografada_bytes = f.encrypt(senha_bytes)
+
+    senha_criptografada_str = senha_criptografada_bytes.decode('utf-8')
+
+
     dados["credenciais"][servico] ={
         "usuario": usuario,
-        "senha": senha_pura
+        "senha": senha_criptografada_str
     }
     salvar_dados(dados)
     print(f"Credencial para '{servico}' adicionada com sucesso.")
@@ -63,3 +82,51 @@ def registrar_senha_mestre(senha_mestre):
     salvar_dados(dados)
     print("Senha mestre registrada com sucesso! Seu cofre de senhas está protegido.")
 
+
+def derivar_char(senha_mestre, salt_hex):
+    #deriva uma chave de 32 bytes
+    salt_bytes = bytes.fromhex(salt_hex)
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt_bytes,
+        iterations=100_000, # Rodar 100 mil vezes para aumentar a segurança contra ataques de força bruta
+    )
+
+    #Gera a chave e codifica em Base64 para uso com Fernet  
+    chave = base64.urlsafe_b64encode(kdf.derive(senha_mestre.encode('utf-8')))
+    return chave
+
+def iniciar_sessao(senha_mestre):
+    global _CHAVE_CRIPTOGRAFICA
+    dados = ler_dados()
+    salt_hex = dados["config"]["salt"]
+    _CHAVE_CRIPTOGRAFICA = derivar_char(senha_mestre, salt_hex)
+
+def buscar_e_descriptografar_senha():
+    global _CHAVE_CRIPTOGRAFICA
+    dados = ler_dados()
+    credenciais = dados.get("credenciais", {})
+
+    if not credenciais or _CHAVE_CRIPTOGRAFICA is None:
+        return credenciais
+    
+    f = Fernet(_CHAVE_CRIPTOGRAFICA)
+    credenciais_descriptografadas = {}
+
+    for servico, info in credenciais.items():
+        try:
+            senha_criptografada_bytes = info["senha"].encode('utf-8')
+            senha_descriptografada_bytes = f.decrypt(senha_criptografada_bytes)
+            senha_descriptografada = senha_descriptografada_bytes.decode('utf-8')
+            credenciais_descriptografadas[servico] = {
+                "usuario": info["usuario"],
+                "senha": senha_descriptografada
+            }
+        except Exception:
+            credenciais_descriptografadas[servico] = {
+                "usuario": info["usuario"],
+                "senha": "⚠️Erro ao descriptografar"
+            }
+
+    return credenciais_descriptografadas
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from gerenciador import (adicionar_senha, 
                          ler_dados,
+                         buscar_e_descriptografar_senha,
                          verificar_senha_mestre, 
-                         registrar_senha_mestre)
+                         registrar_senha_mestre,
+                         iniciar_sessao)
 
 def exibir_menu():
     print("\n" + "=" * 30)
@@ -25,6 +27,7 @@ def autenticar_usuario():
 
             if senha == confirmar_senha and senha != "":
                 registrar_senha_mestre(senha)
+                iniciar_sessao(senha)
                 break
             else:
                 print("As senhas nao coincidem ou estao vazias. Tente novamente.")
@@ -35,6 +38,7 @@ def autenticar_usuario():
             senha_digitada = input("Digite sua SENHA MESTRE para acessar: ").strip()
             if verificar_senha_mestre(senha_digitada):
                 print("Acesso concedido! Bem-vindo de volta.")
+                iniciar_sessao(senha_digitada)
                 return True
             else:
                 tentativas -= 1
@@ -61,7 +65,7 @@ def main():
                 print("⚠️Todos os campos são obrigatórios. Tente novamente.")
         elif escolha == "2":
             dados = ler_dados()
-            credenciais = dados.get("credenciais", {})
+            credenciais = buscar_e_descriptografar_senha()  # Agora retorna as senhas descriptografadas
             
             if not credenciais:
                 print("\n📭 Nenhum serviço cadastrado ainda.")
